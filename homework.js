24910067b19ede1717667463e6a192905d1d72f3
Zubarev Sergey <barclays13@mail.ru>
2019-10-29 23:39:02 +0300

    changes1

--- homework.js
+++ homework.js
@@ -10,23 +10,21 @@ const sponsors = {
     cash: [40000, 5000, 30400, 12000],
     eu: ['SRL', 'PLO', 'J&K'],
     rus: ['RusAuto', 'SBO'],
-    calcCash () {
-        let total = 0;
-        this.cash.forEach((item)=>{
-            total += item;
-        });        
-        return total;
-    }
 };
 
 const {cash, eu, rus} = sponsors;
+const {cash: [cash0, cash1, cash2, cash3], eu: [eu0, eu1, eu2], rus: [rus0, rus1]} = sponsors;
 
-makeBusiness = (owner, director = 'Victor', cash, emp) => {
-    let sumSponsors = eu.concat(rus, 'unexpected sponsor');
 
-    console.log(`We have a business. Owner: ${owner}, director: ${director}. Our budget: ${cash}. And our employers: ${emp}
+const calcCash = (cash = 0) => cash.reduce((a, b) => a + b);
+const money = calcCash(sponsors.cash);
+
+const sumSponsors = [...eu, ...rus, 'unexpected sponsor']; 
+
+makeBusiness = (owner, director="Victor", cash, emp) => {
+console.log(`We have a business. Owner: ${owner}, director: ${director}. Our budget: ${cash}. And our employers: ${emp}
 And we have a sponsors: 
 ${sumSponsors}
-Note. Be careful with ${eu[0]}. It's a huge risk.`);
+Note. Be careful with ${eu0}. It's a huge risk.`);
 }
-makeBusiness.apply(null, ['Sam', null, sponsors.calcCash(), employersNames]);
+makeBusiness.apply(null, ['Sam', , money, employersNames]);
