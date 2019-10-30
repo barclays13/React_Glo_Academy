e0821a124edd93d979f9d5a041292ee02e76d063
Zubarev Sergey <barclays13@mail.ru>
2019-10-30 20:39:54 +0300

    changes2

--- homework.js
+++ homework.js
@@ -18,13 +18,22 @@ const {cash: [cash0, cash1, cash2, cash3], eu: [eu0, eu1, eu2], rus: [rus0, rus1
 
 const calcCash = (cash = 0) => cash.reduce((a, b) => a + b);
 const money = calcCash(sponsors.cash);
+console.log('money: ', money);
 
 const sumSponsors = [...eu, ...rus, 'unexpected sponsor']; 
 
-makeBusiness = (owner, director="Victor", cash, emp) => {
-console.log(`We have a business. Owner: ${owner}, director: ${director}. Our budget: ${cash}. And our employers: ${emp}
+function makeBusiness ({ 
+    owner = 'Samen',
+    director="Victor",
+    cash = 0,
+    emp = ['ludmila', 'viktor']}) {
+        console.log(`We have a business. Owner: ${owner}, director: ${director}. Our budget: ${cash}. And our employers: ${emp}
 And we have a sponsors: 
 ${sumSponsors}
 Note. Be careful with ${eu0}. It's a huge risk.`);
 }
-makeBusiness.apply(null, ['Sam', , money, employersNames]);
+makeBusiness({
+    cash:money,
+    emp: employersNames,
+    owner: 'Sam'
+});
